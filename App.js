6a26682ce5e540487d8ea722e4faae221471d497
Elrngit <praveena.beerla@gmail.com>
2026-09-06 20:20:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+
+import './App.css';
+import 'bootstrap.min.css';
+import 'bootstrap.bundle.min.js';
+import {Routes, Route} from "react-router";
+import Testnwcnt from './Testnwcnt';
+
+function App() {
+  
+
+return (
+   
+
+<div className="App">
+
+<Routes>
+
+<Route path="/" Component={Testnwcnt}/>
+
+</Routes>
+</div> 
+
+);
+
+
+}
+
+
+
+
+
+export default App;
---
+++ Testnwcnt.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+
+const Testnwcnt=()=>{
+
+
+
+
+return(
+<div>
+<section class="container-fluid bg-primary text-center">
+<h2 class="my-5 mb-4">  Contact </h2>
+<div class="container my-5">
+<div class="row">
+<div class="col-md-6">
+<h2>  Send Msg </h2>
+<form>
+<input type="text"/>
+<input type="submit" value="submit"/>
+</form>
+</div>
+<div class="col-md-6">
+<h2> Address </h2>
+<p>  Addressline1 </p>
+<p>  Addressline2 </p>
+<h2> Flw </h2>
+<a href="Infrm"> Infrm </a>
+</div>
+</div>
+</div>
+</section>
+</div>
+
+)
+
+}
+export default Testnwcnt
